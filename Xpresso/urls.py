9cccc53bcd17ad16edfac30bcbdd6508090cd3b3
Jared Winter <jwinternet@outlook.com>
2024-02-26 14:18:01 -0500

    Completed chapter 2.

--- Xpresso/urls.py
+++ Xpresso/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('horror_show/', include('horror_show.urls', namespace='horror_show')),
+    path("", include("horror_show.urls"))
 ]
